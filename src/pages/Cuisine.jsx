897e47b364ffa71951005f5255f20e812b2fdff1
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-05 02:24:33 +0900

    feat: Cuisine에 useParams추가
    - Category의 각 NavLink를 누르면, :type값이 변경되어 Cuisine.jsx파일 안에있는 useEffect가 실행됨
    - 이 때 useEffect는 getCuisine함수를 실행시키는데, :type값을 인자로 전달해주어 url에 동적으로 반영됨.

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import {Link, useParams} from 'react-router-dom'
+
+
 
 function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async(name)=>{
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`); // :type 자리에 쓰는게 name에 전달되어 들어간다
+    const recipes = await data.json();
+    setCuisine(recipes.results)
+  };
+
+  useEffect(()=>{
+    // params의 type이 변할때마다 useEffect도 다시 랜더링되게 해줘
+    getCuisine(params.type)
+    console.log(params.type);
+  },[params.type])
+
   return (
-    <div>Cuisine</div>
+    <div>
+    </div>
   )
 }
 
