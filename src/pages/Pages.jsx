0e714f4cb7cb1ad7ea108fc87240b4cf21b1f66a
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-05 13:45:07 +0900

    feat: Searched 페이지 추가 후 라우트 적용
    - /searched/:search로 접속시 Searched컴포넌트 출력

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Home from './Home'
+import Searched from './Searched'
 import Cuisine from './Cuisine'
 import {Route, Routes} from 'react-router-dom'
 
@@ -8,6 +9,7 @@ function Pages() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/cuisine/:type' element={<Cuisine/>}></Route>
+        <Route path='/searched/:search' element={<Searched/>}></Route>
       </Routes>
   )
 }
