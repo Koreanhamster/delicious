324eece7d26d24749744e4f85d6ef8ba22c2b0d1
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-05 10:25:50 +0900

    feat: Cuisine 타입별 데이터 출력
    - Cuisine type별로 받아온 데이터 맵을 사용하여 리턴
    - 스타일 수정: 그리드 사용

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -8,23 +8,54 @@ import {Link, useParams} from 'react-router-dom'
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
   let params = useParams();
+  console.log('얍');
 
   const getCuisine = async(name)=>{
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`); // :type 자리에 쓰는게 name에 전달되어 들어간다
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}&number=9`);
     const recipes = await data.json();
     setCuisine(recipes.results)
+    console.log(recipes.results);
   };
 
   useEffect(()=>{
-    // params의 type이 변할때마다 useEffect도 다시 랜더링되게 해줘
     getCuisine(params.type)
     console.log(params.type);
   },[params.type])
 
   return (
-    <div>
-    </div>
+    <Grid>
+      {cuisine.map((item)=>{
+        return(
+          <Card key={item.id}>
+            <img src={item.image} alt=''/>
+            <h4>{item.title}</h4>
+          </Card>
+        )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img{
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  h4{
+    text-align: center;
+    padding: 1rem;
+  }
+`
 
 export default Cuisine 
