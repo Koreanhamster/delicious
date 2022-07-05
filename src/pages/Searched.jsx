87fda1d4aea8e5514a44c4c7dff1d2af8069d138
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-05 14:26:14 +0900

    feat: fetch된 searchedRecipes 화면에 출력
    - useEffect사용하여 url의 /:search가 바뀔 때 마다(params.search) getSearched(params.search)함수를 실행시킴
    - 함수는 전달받은 인자값을 fetch할 때 쿼리에 넣고 데이터를 가져옴
    - map을 사용해서 화면에 데이터 출력
    - CSS 스타일링

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import styled  from 'styled-components';
 
 function Searched() {
+
+  const [searchedRecipes, setSearchedRecipes] = useState([])
+  let params = useParams();
+
+  const getSearched = async(name)=>{
+    // 이번엔 쿼리로 아무 문자나 넣을 수 있게 하기
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}&number=9`);
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results)
+    console.log(recipes.results);
+  };
+
+  useEffect(()=>{
+    getSearched(params.search);
+    // 업데이트 될 때마다 다시 fetch해주기
+    // params. 찍고 type하면 :type이 들어가고 .search하면 :search가 들어감.
+  },[params.search])
+
   return (
-    <div>Searched</div>
+    <Grid>
+      {searchedRecipes.map((item)=>{
+        return(
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img{
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  h4{
+    text-align: center;
+    padding: 1rem;
+  }
+`
 
 export default Searched
